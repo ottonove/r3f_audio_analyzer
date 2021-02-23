4a3ac00873d3bd3ff6561dea211b97fe8ebb94f5
ottonove <niigata01web@gmail.com>
2021-02-23 17:14:37 +0900

    マウスダウンではなく、ラジオボタンの変更のみでコードを切り替えようとしているが、切り替えても、前のコードの音も鳴り続ける。

--- src/Arpeggiator.js
+++ src/Arpeggiator.js
@@ -2,76 +2,70 @@ import React, { useEffect, useState } from "react";
 import * as Tone from 'tone'
 
 export default function Arpeggiator() {
-  const [started, setStarted] = useState(false);
+
+  const [chord, setChord] = useState(0);
 
   useEffect(() => {
-    document.documentElement.addEventListener('mousedown', () => {
-      if (started) return;
 
-      setStarted(true);
-    
-      const $inputs = document.querySelectorAll('input'),
-            chords = [
-              'A0 C1 E1', 'F0 A0 C1', 'G0 B0 D1',
-              'D0 F0 A0', 'E0 G0 B0'
-            ].map(formatChords);
-      console.log('コーズ:', chords);
-      /*
-      0: (6) ["A4", "C5", "E5", "A5", "C6", "E6"]
-      1: (6) ["F4", "A4", "C5", "F5", "A5", "C6"]
-      2: (6) ["G4", "B4", "D5", "G5", "B5", "D6"]
-      3: (6) ["D4", "F4", "A4", "D5", "F5", "A5"]
-      4: (6) ["E4", "G4", "B4", "E5", "G5", "B5"]
-      */
-      var chordIdx = 0,
-          step = 0;
-    
-      const synth = new Tone.Synth();
-      const gain = new Tone.Gain(0.7);
-      synth.oscillator.type = 'triangle';
-      gain.toDestination();
-      synth.connect(gain);
-    
-      Array.from($inputs).forEach($input => {
-        $input.addEventListener('change', () => {
-          console.log('$input.value:', $input.value);
-          if ($input.checked) handleChord($input.value);
-        })
-      });
-    
-      function handleChord(valueString) {
-        chordIdx = parseInt(valueString) - 1;
-      }
-    
-      Tone.Transport.scheduleRepeat(onRepeat, '16n');
-      Tone.Transport.start();
-      Tone.Transport.bpm.value = 90;
-    
-      function onRepeat(time) {
-        console.log('time:',time);//この関数の実行開始からの経過時間を引数に持つ
-        let chord = chords[chordIdx],
-            note = chord[step % chord.length];
-        synth.triggerAttackRelease(note, '16n', time);
-        step++;
-      }
-    
-      // DOWN THE LINE THIS WILL MAKE THINGS EASIER
-      function formatChords(chordString) {
-        let chord = chordString.split(' ');
-        let arr = [];
-        for (let i = 0; i < 2; i++) {
-          for (let j = 0; j < chord.length; j++) {
-            let noteOct = chord[j].split(''),
-                note = noteOct[0];
-            let oct = (noteOct[1] === '0') ? i + 4 : i + 5;
-            note += oct;
-            arr.push(note);
-          }
+    console.log('useEffect fire!');
+
+    const $inputs = document.querySelectorAll('input'),
+    chords = [
+      'A0 C1 E1', 'F0 A0 C1', 'G0 B0 D1',
+      'D0 F0 A0', 'E0 G0 B0'
+    ].map(formatChords);
+      
+    let chordIdx = 0,
+        step = 0;
+  
+    const synth = new Tone.Synth();
+    const gain = new Tone.Gain(0.7);
+    synth.oscillator.type = 'triangle';
+    gain.toDestination();
+    synth.connect(gain);
+  
+    Array.from($inputs).forEach($input => {
+      $input.addEventListener('change', () => {
+        // console.log('$input.value:', $input.value);
+        if ($input.checked) handleChord($input.value);
+      })
+    });
+  
+    function handleChord(valueString) {
+      chordIdx = parseInt(valueString) - 1;
+      setChord(chordIdx);
+    }
+  
+    Tone.Transport.scheduleRepeat(onRepeat, '16n');
+    Tone.Transport.start();
+    Tone.Transport.bpm.value = 90;
+  
+    function onRepeat(time) {
+      //この関数の実行開始からの経過時間を引数に持つ
+      let chord = chords[chordIdx],
+          note = chord[step % chord.length];
+      synth.triggerAttackRelease(note, '16n', time);
+      step++;
+    }
+  
+    // DOWN THE LINE THIS WILL MAKE THINGS EASIER
+    function formatChords(chordString) {
+      let chord = chordString.split(' ');
+      let arr = [];
+      for (let i = 0; i < 2; i++) {
+        for (let j = 0; j < chord.length; j++) {
+          let noteOct = chord[j].split(''),
+              note = noteOct[0];
+          let oct = (noteOct[1] === '0') ? i + 4 : i + 5;
+          note += oct;
+          arr.push(note);
         }
-        return arr;
       }
-    },{once:true});
-  });
+      return arr;
+    }
+
+    console.log('Tone state:', Tone.context.state);
+  },[chord]);
 
   return (
     <React.Fragment>

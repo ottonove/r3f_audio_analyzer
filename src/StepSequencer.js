8b0a4a9d8efea1d294477767a891b6dd0053a502
ottonove <niigata01web@gmail.com>
2021-02-23 09:34:06 +0900

    stepSequencerを実装中。

--- src/StepSequencer.js
+++ src/StepSequencer.js
@@ -3,57 +3,48 @@ import * as Tone from 'tone'
 
 export default function StepSequencer() {
     useEffect(() => {
-      Tone.Transport.bpm.value = 180;
-  
-      //シンセサイザーインスタンス
-      const melody_synth = new Tone.Synth().toDestination();
-  
-      // 楽譜データ
-      const melody_score = [
-        //note(音名) nullは休符
-        [{ note: "C5" }, { note: "C5" }],
-        [{ note: "C5" }, { note: "C5" }],
-        [null, null],
-        [{ note: "G#4" }, null],
-        [null, { note: "A#4" }],
-        [null, null],
-        [{ note: "C5" }, null],
-        [{ note: "A#4" }, { note: "C5" }]
+      document.documentElement.addEventListener('mousedown', () => {
+        if (Tone.context.state !== 'running') Tone.context.resume();
+      });
+        
+      const synths = [
+        new Tone.Synth(),
+        new Tone.Synth(),
+        new Tone.Synth()
       ];
-  
-      //シーケンサーインスタンス
-      const melody_sequence = new Tone.Sequence(
-        (time, { note }) => {
-          melody_synth.triggerAttackRelease(note);
-        },
-        melody_score,
-        "4n"
-      ).start();
-  
-      //演奏のリピートをfalseに
-      melody_sequence.loop = false;
+      
+      synths[0].oscillator.type = 'triangle';
+      synths[1].oscillator.type = 'sine';
+      synths[2].oscillator.type = 'sawtooth';
+      
+      const gain = new Tone.Gain(0.6);
+      gain.toMaster();
+      
+      synths.forEach(synth => synth.connect(gain));
+      
+      const $rows = document.body.querySelectorAll('div > div'),
+            notes = ['G5', 'E4', 'C3'];
+      let index = 0;
+      
+      Tone.Transport.scheduleRepeat(repeat, '8n');
+      Tone.Transport.start();
+      
+      function repeat(time) {
+        let step = index % 8;
+        for (let i = 0; i < $rows.length; i++) {
+          let synth = synths[i],
+              note = notes[i],
+              $row = $rows[i],
+              $input = $row.querySelector(`input:nth-child(${step + 1})`);
+          if ($input.checked) synth.triggerAttackRelease(note, '8n', time);
+        }
+        index++;
+      }
   
     },[]);
   
-    const musicStart = () => {
-      if(Tone.context.state === 'suspended') {
-        Tone.context.resume()
-        .then(() => {
-          Tone.Transport.start();
-        });
-      } else {
-        Tone.Transport.start();
-      }
-    }
-  
-    const musicStop = () => {
-      Tone.Transport.stop();
-    }
-  
     return (
       <div>
-        <button onClick={musicStart}>start</button>
-        <button onClick={musicStop}>stop</button>
       </div>
     );
   }

19773d7029557e2657057576200d50f07c0494b2
ottonove <niigata01web@gmail.com>
2021-02-23 10:55:44 +0900

    querySelectorでの要素の参照方法が誤っていたので修正した。

--- src/StepSequencer.js
+++ src/StepSequencer.js
@@ -22,21 +22,26 @@ export default function StepSequencer() {
       
       synths.forEach(synth => synth.connect(gain));
       
-      const $rows = document.body.querySelectorAll('div > div'),
+      const $rows = document.body.querySelectorAll('.seq'),
             notes = ['G5', 'E4', 'C3'];
       let index = 0;
       
-      Tone.Transport.scheduleRepeat(repeat, '8n');
+      Tone.Transport.scheduleRepeat(loop, '8n');
       Tone.Transport.start();
       
-      function repeat(time) {
+      function loop(time) {
         let step = index % 8;
         for (let i = 0; i < $rows.length; i++) {
           let synth = synths[i],
               note = notes[i],
               $row = $rows[i],
               $input = $row.querySelector(`input:nth-child(${step + 1})`);
-          if ($input.checked) synth.triggerAttackRelease(note, '8n', time);
+          if ($input.checked) {
+            console.log('$rows:',$rows);
+            console.log('i:',i);
+            console.log(synth.oscillator.type);
+            synth.triggerAttackRelease(note, '8n', time);
+          }
         }
         index++;
       }
@@ -44,7 +49,37 @@ export default function StepSequencer() {
     },[]);
   
     return (
-      <div>
-      </div>
+      <React.Fragment>
+        <div className="seq">
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+        </div>
+        <div  className="seq">
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+        </div>
+        <div  className="seq">
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+        </div>
+      </React.Fragment>
     );
   }
